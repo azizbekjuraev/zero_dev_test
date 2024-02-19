426c2db77db422fddcc869dc6b53bf617908e77a
AZIZBEKJURAEV <ajuraev98@mail.ru>
2024-02-20 00:54:23 +0500

    more changes added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,19 @@
 import "./App.css";
+import Graph from "./components/Graph";
 
 export default function App() {
   return (
     <div className="App">
-      <h1 className="font-bold">Личный финансовый учет</h1>
+      <div className="container mx-auto max-w-6xl text-center drop-shadow-lg text-gray-800">
+        <h1 className="text-xl p-8 mb-10 bg-slate-800 text-white rounded">
+          Личный финансовый учет
+        </h1>
+        {/* grid columns */}
+        <div className="grid md:grid-cols-2 gap-4"></div>
+        {/* Chart */}
+        <Graph></Graph>
+        {/* Form */}
+      </div>
     </div>
   );
 }
---
+++ client/src/components/Graph.jsx
@@ -0,0 +1,57 @@
+import styled from "styled-components";
+import { Chart, ArcElement } from "chart.js";
+import { Doughnut } from "react-chartjs-2";
+import Labels from "./Labels";
+
+Chart.register(ArcElement);
+
+const StyledChartTitle = styled.h3`
+  position: absolute;
+  left: 0;
+  right: 0;
+  top: 40%;
+  margin-left: auto;
+  margin-right: auto;
+`;
+
+export default function Graph() {
+  const config = {
+    data: {
+      datasets: [
+        {
+          label: "My First Dataset",
+          data: [300, 50, 100],
+          backgroundColor: [
+            "rgb(255, 99, 132)",
+            "rgb(54, 162, 235)",
+            "rgb(255, 205, 86)",
+          ],
+          hoverOffset: 4,
+          borderRadius: 30,
+          spacing: 10,
+        },
+      ],
+    },
+    options: {
+      cutout: 115,
+    },
+  };
+
+  console.log(typeof config.data.datasets[0].backgroundColor);
+
+  return (
+    <div className="flex justify-content max-w-xs mx-auto">
+      <div className="item ">
+        <div className="chart relative">
+          <Doughnut {...config}></Doughnut>
+          <StyledChartTitle className="mb-4 font-bold title">
+            Total <span className="block text-3xl text-emerald-400">${0}</span>
+          </StyledChartTitle>
+        </div>
+        <div className="flex flex-col py-10 gap-4">
+          <Labels></Labels>
+        </div>
+      </div>
+    </div>
+  );
+}

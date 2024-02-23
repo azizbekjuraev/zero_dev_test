12bbf473c4e5f1235a7451c0eaec6a3baba54ef6
AZIZBEKJURAEV <ajuraev98@mail.ru>
2024-02-24 00:13:36 +0500

    ui change

--- client/src/components/Transaction.jsx
+++ client/src/components/Transaction.jsx
@@ -6,6 +6,8 @@ export default function Transaction({ category, handle }) {
       className="item flex justify-center bg-gray-50 py-2"
       style={{ borderRight: `8px solid ${color ?? "black"}` }}
     >
+      <span className="block w-full">{name}</span>
+      <span className="block w-full text-zinc-500">${amount}</span>
       <button className="px-3" onClick={handle}>
         <box-icon
           data-id={_id ?? ""}
@@ -14,8 +16,6 @@ export default function Transaction({ category, handle }) {
           type="solid"
         ></box-icon>
       </button>
-      <span className="block w-full">{name}</span>
-      <span className="block w-full text-zinc-500">${amount}</span>
     </div>
   );
 }
